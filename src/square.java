bde7341fd4d8ac866f5d13b4de50978354fd7309
Anton Fedoseev <120740153+faa92@users.noreply.github.com>
2022-12-28 17:58:16 +0300

    Add files via upload

--- src/square.java
+++ src/square.java
@@ -1,6 +1,11 @@
 public class square {
 
     public static void main (String[] args) {
+        int a = 0;
+        for (a = 10; a < 21; a++) {
+            System.out.println("Результат: " + a*a);
+
+        }
 
 
 
